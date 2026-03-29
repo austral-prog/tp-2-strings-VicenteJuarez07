c93553f8f3bf800bcddd15b3f0a3ebe09cf1ed56
VicenteJuarez07 <vjuarez@mail.austral.edu.ar>
2026-03-28 22:18:38 -0300

    Add files via upload

--- casting.py
+++ casting.py
@@ -1,5 +1,6 @@
 def casting():
     """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
+    pass
 
     # ________________________________________RESOLUCION EJERCICIO 3_______________________________________
 
