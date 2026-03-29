e1748523313b0cc8d85463581b547cbbf27c9ad8
VicenteJuarez07 <vjuarez@mail.austral.edu.ar>
2026-03-29 18:28:41 -0300

    Add files via upload

--- string_methods.py
+++ string_methods.py
@@ -1,6 +1,8 @@
 def string_methods():
-    #Demuestra el uso de métodos de string: strip, lstrip, rstrip, upper, lower, title, find, replace, count, operador in, slicing con paso, reverso, f-strings y strings multilínea.
-
+    """Demuestra el uso de métodos de string: strip, lstrip, rstrip, upper, lower,
+    title, find, replace, count, operador in, slicing con paso, reverso,
+    f-strings y strings multilínea.
+    """
     nombre = "   Grace Hopper   "
     frase = "Python es un gran lenguaje de programacion"
     multilinea = """Linea 1
